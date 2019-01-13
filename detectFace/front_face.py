7b294d20eecf5ed2da099265f76ba2024abc92e3
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2019-01-13 15:31:40 +0530

    Front face detect

---
+++ detectFace/front_face.py
@@ -0,0 +1,24 @@
+import cv2 
+
+#Pre trained model
+face_model = cv2.CascadeClassifier('../models/haarcascade_frontalface_alt2.xml')
+video = cv2.VideoCapture(0)
+count = 1
+while(True):
+    status, frame = video.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_model.detectMultiScale(gray)
+    for (x, y, w, h) in faces:
+        print('x: %s| y: %s | w: %s| h: %s'%(x,y,w,h))
+        #BGR Color Frame
+        color = (0, 255, 0) 
+        rectangle_thickness = 2
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color, rectangle_thickness)
+
+    cv2.imshow('captching ...', frame)
+    key = cv2.waitKey(3)
+    if key == ord('q'):
+        break
+    
+video.release()
+cv2.destroyAllWindows()
